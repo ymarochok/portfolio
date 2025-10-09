9d9e0b6895b5cc3c022027919b87ee11e2b9db0c
Yaroslav Marochok <ymarochok@gmail.com>
2025-10-09 12:23:25 +0200

    update directory of assets

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -14,7 +14,7 @@ export default function Footer() {
                     <a href="https://github.com/ymarochok" target="_blank" rel="noopener noreferrer" aria-label="GitHub">
                         {/* <FontAwesomeIcon icon={faGithub} size="2x" /> */}
                         <img 
-                            src="/github.png" 
+                            src="./assets/github.png" 
                             alt="GitHub" 
                             // style={{ width: "48px", height: "48px"}} 
                             />
@@ -24,7 +24,7 @@ export default function Footer() {
                     <a href="https://www.linkedin.com/in/yaroslav-marochok-7b495a358/" target="_blank" rel="noopener noreferrer" aria-label="LinkedIn">
                         {/* <FontAwesomeIcon icon={faLinkedin} size="2x" /> */}
                         <img 
-                            src="/linkedin.png" 
+                            src="./assets/linkedin.png" 
                             alt="Linkedin" 
                             // style={{ width: "48px", height: "48px"}} 
                             />
@@ -33,7 +33,7 @@ export default function Footer() {
                     <a href="https://t.me/@ukr_og_rick" target="_blank" rel="noopener noreferrer" aria-label="Telegram">
                         {/* <FontAwesomeIcon icon={faTelegram} size="2x" /> */}
                         <img 
-                            src="/telegram.png" 
+                            src="./assets/telegram.png" 
                             alt="Telegram" 
                             // style={{ width: "48px", height: "48px"}} 
                             />
@@ -42,7 +42,7 @@ export default function Footer() {
                     <a href="mailto:ymarochok@gmail.com" aria-label="Email">
                         {/* <FontAwesomeIcon icon={faEnvelope} size="2x" /> */}
                         <img 
-                            src="/envelope.png" 
+                            src="./assets/envelope.png" 
                             alt="Gmail" 
                             style={{ width: "32px", height: "22px"}} 
                             />
@@ -51,7 +51,7 @@ export default function Footer() {
                     <a href="tel:+421952155913" aria-label="Phone">
                         {/* <FontAwesomeIcon icon={faPhone} size="2x" /> */}
                         <img 
-                            src="/phone.png" 
+                            src="./assets/phone.png" 
                             alt="Phone" 
                             // style={{ width: "48px", height: "48px"}} 
                             />

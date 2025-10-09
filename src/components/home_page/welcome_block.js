0d630825fde243bb52e79123ba4387560f43c13c
Yaroslav Marochok <ymarochok@gmail.com>
2025-10-09 14:14:35 +0200

    responsive style update

--- src/components/home_page/welcome_block.js
+++ src/components/home_page/welcome_block.js
@@ -5,13 +5,14 @@ export default function Welcome_block(){
 
     return(
         <div className="main">
-
+            <div>
             <h1>Welcome!</h1>
             <p>Welcome to my portfolio web-page. My name is Yaroslav Marochok.</p>
 
             <div className="row">
                 <button > About Me </button>
                 <button onClick={() => navigate('/projects')}> Projects </button>    
+            </div>
             </div>
         </div>
     )

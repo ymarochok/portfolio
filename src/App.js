5c82adae6de16e639c0faba83d274ef4d430f07b
Yaroslav Marochok <ymarochok@gmail.com>
2025-10-14 00:55:36 +0200

    Parallax effect update

--- src/App.js
+++ src/App.js
@@ -8,37 +8,45 @@ import Projects_page from './components/projects_page/Projects_page';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 
-function App() {
+function CursorEffect() {
+  const location = useLocation(); 
 
   useEffect(() => {
-        
-        const ids = ["main", "skills", "app"];
-        const elements = ids
-            .map(id => document.getElementById(id))
-            .filter(Boolean); 
+    const ids = ["main", "skills", "app"];
+    const elements = ids
+      .map(id => document.getElementById(id))
+      .filter(Boolean);
 
-        function handleMove(e) {
-            const { innerWidth, innerHeight } = window;
-            const moveX = (e.clientX - innerWidth / 2) / innerWidth * 20;
-            const moveY = (e.clientY - innerHeight / 2) / innerHeight * 20;
+    function handleMove(e) {
+      const { innerWidth, innerHeight } = window;
+      const moveX = (e.clientX - innerWidth / 2) / innerWidth * 10;
+      const moveY = (e.clientY - innerHeight / 2) / innerHeight * 20;
 
-            for (const el of elements) {
-            el.style.transform = `translate(${moveX}px, ${moveY}px)`;
-            el.style.transition = "transform 0.1s ease-out";
-            }
-        }
+      for (const el of elements) {
+        el.style.transform = `translate(${moveX}px, ${moveY}px)`;
+        el.style.transition = "transform 0.1s ease-out";
+      }
+    }
 
-        document.addEventListener("mousemove", handleMove);
-        return () => document.removeEventListener("mousemove", handleMove);
-    }, []);
+    document.addEventListener("mousemove", handleMove);
+    return () => document.removeEventListener("mousemove", handleMove);
+  }, [location.pathname]); 
+
+  return null; 
+}
+
+
+
+function App() {
 
   return (
     <div className='app' id='app'>
 
     <BrowserRouter>
       <Header />
-
+      <CursorEffect />
       <Routes>
         <Route path="/portfolio" element={<Home />} />
         <Route path="/projects" element={<Projects_page />} />
